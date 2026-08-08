5574584f51483cf74d5077f9f369b3eebca09418
alinikfarjam79 <alinikitest@gmail.com>
2026-08-08 16:37:51 +0330

    fix cors

--- src/app.js
+++ src/app.js
@@ -16,7 +16,7 @@ app.use(helmet());
 
 app.use(
   cors({
-    origin: env.clientUrl,
+    origin: true,
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
